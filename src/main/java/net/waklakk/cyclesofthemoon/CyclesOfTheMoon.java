0fb57fd2f228835da49c585f82796ebb893665ca
Max <max@the-barkers.com>
2022-11-27 17:17:39 +0000

    Initial commit

---
+++ src/main/java/net/waklakk/cyclesofthemoon/CyclesOfTheMoon.java
@@ -0,0 +1,15 @@
+package net.waklakk.cyclesofthemoon;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CyclesOfTheMoon implements ModInitializer {
+    public static final String MOD_ID = "cycles-of-the-moon";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/waklakk/cyclesofthemoon/CyclesOfTheMoonClient.java
@@ -0,0 +1,11 @@
+package net.waklakk.cyclesofthemoon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class CyclesOfTheMoonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
